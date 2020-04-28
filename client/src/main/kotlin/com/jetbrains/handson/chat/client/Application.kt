a4c56b47c8a96ff645be8a5d3402c7c5c9efb95a
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-04-28 15:40:33 +0200

    Replace compareTo with equals

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/Application.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/Application.kt
@@ -4,29 +4,27 @@ import io.ktor.client.*
 import io.ktor.client.features.websocket.*
 import io.ktor.http.*
 import io.ktor.http.cio.websocket.*
-import javafx.scene.control.Tooltip.*
 import kotlinx.coroutines.*
-import kotlin.system.*
 
 fun main() = runBlocking {
     val client = HttpClient {
         install(WebSockets)
     }
-        client.webSocket(
-                method = HttpMethod.Get,
-                host = "0.0.0.0",
-                port = 8080, path = "/chat"
-        ) {
-            var message = readLine() ?: ""
-            while (message.compareTo("exit", true) != 0) {
-                send(message)
-                when (val frame = incoming.receive()) {
-                    is Frame.Text -> println(frame.readText())
-                    is Frame.Binary -> println(frame.readBytes())
-                }
-                message = readLine() ?: ""
+    client.webSocket(
+            method = HttpMethod.Get,
+            host = "0.0.0.0",
+            port = 8080, path = "/chat"
+    ) {
+        var message = readLine() ?: ""
+        while (!message.equals("exit", true)) {
+            send(message)
+            when (val frame = incoming.receive()) {
+                is Frame.Text -> println(frame.readText())
+                is Frame.Binary -> println(frame.readBytes())
             }
+            message = readLine() ?: ""
         }
-        client.close()
-        println("Connection closed")
+    }
+    client.close()
+    println("Connection closed")
 }
